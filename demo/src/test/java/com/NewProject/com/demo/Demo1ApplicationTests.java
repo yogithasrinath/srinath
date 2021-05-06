d589544e6281947acbddbd7cc202fb81fd892bab
yogitha <ksyogithalakshmi54@gmail.com>
2021-05-06 20:31:57 +0530

    yogitha

---
+++ demo/src/test/java/com/NewProject/com/demo/Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NewProject.com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
